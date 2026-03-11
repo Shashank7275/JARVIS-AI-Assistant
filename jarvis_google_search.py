b1791f3a2903d794008ea47a2479ea5c9c3f7f8c
Shashank Singh <shashanks72754791@gmail.com>
2026-03-11 23:29:58 +0530

    Update jarvis_google_search.py

--- jarvis_google_search.py
+++ jarvis_google_search.py
@@ -1,6 +1,6 @@
 import os
 import sys
-import request
+import requests
 import logging
 from dotenv import load_dotenv
 from livekit.agents import function_tool
@@ -60,6 +60,7 @@ async def google_search(query: str) -> str:
 @function_tool
 async def get_current_datetime() -> str:
     return datetime().now.isoformate()
+
 
 
 
